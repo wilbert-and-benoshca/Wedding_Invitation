5d7d52bca948dec8d2730664d25b5de8f77dec13
wilbert-and-benoshca <wilbertcardoso229@gmail.com>
2026-09-14 02:10:48 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  // Paste your Apps Script Web App Deployment URL here
+  API_URL: "https://script.google.com/macros/s/AKfycbxibqA6rUmKxoZKumraWKrnlIkH9RyGGh2nW5-OHFfDa0CMN7nZzJm0--KViyjAunIi/exec",
+  
+  WEDDING_DATE: "December 5, 2026 15:30:00",
+  IMAGES: {
+    logo: "https://drive.google.com/thumbnail?id=14VpY2F_7WJbM8uQjR0L9fI3g_N123456&sz=w1000", // or upload your gold WB monogram to repo
+    corner1: "https://drive.google.com/thumbnail?id=1uhz1T3ID-FuFDdSredRLyc6WZ7aDwfN4&sz=w500",
+    corner2: "https://drive.google.com/thumbnail?id=1uhz1T3ID-FuFDdSredRLyc6WZ7aDwfN4&sz=w500",
+    churchBg: "https://images.unsplash.com/photo-1548625361-195fe578ae1b?w=800",
+    venueBg: "https://images.unsplash.com/photo-1519167758481-83f550bb49b3?w=800"
+  }
+};
