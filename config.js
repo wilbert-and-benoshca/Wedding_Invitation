3621309ef6765ec6b2b94c5166558ffd56e9f4e0
wilbert-and-benoshca <wilbertcardoso229@gmail.com>
2026-09-14 03:47:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,42 @@ const CONFIG = {
   // Paste your Apps Script Web App Deployment URL here
   API_URL: "https://script.google.com/macros/s/AKfycbxibqA6rUmKxoZKumraWKrnlIkH9RyGGh2nW5-OHFfDa0CMN7nZzJm0--KViyjAunIi/exec",
   
-  WEDDING_DATE: "December 5, 2026 15:30:00",
+  COUPLE: {
+    groom: "Wilbert",
+    bride: "Benoshca"
+  },
+
+  WEDDING: {
+    date: "December 5, 2026",
+    countdownDate: "December 5, 2026 15:30:00",
+    time: "3:30 PM",
+    venue: "Our Lady of Consolation of the Persecuted Church",
+    address: "Tropa Vaddo, Sodiem, Siolim, Bardez, Goa 403517",
+    mapUrl: "https://maps.google.com/?q=Our+Lady+of+Consolation+of+the+Persecuted+Church+Siolim",
+    verse: '"SO THEY ARE NO LONGER TWO, BUT ONE. THEREFORE WHAT GOD HAS JOINED TOGETHER, LET MAN NOT SEPARATE." - MATTHEW 19:6'
+  },
+
+  RECEPTION: {
+    time: "7:00 PM Sharp",
+    venue: "Silver Bells",
+    address: "Off the Highway, Sangolda, Bardez, Goa 403511",
+    mapUrl: "https://maps.google.com/?q=Silver+Bells+Sangolda+Goa"
+  },
+
+  CONTACT: {
+    phone: "+91 95183 64511",
+    email: "wilbertcardoso229@gmail.com"
+  },
+
   IMAGES: {
-    logo: "https://drive.google.com/thumbnail?id=14VpY2F_7WJbM8uQjR0L9fI3g_N123456&sz=w1000", // or upload your gold WB monogram to repo
-    corner1: "https://drive.google.com/thumbnail?id=1uhz1T3ID-FuFDdSredRLyc6WZ7aDwfN4&sz=w500",
-    corner2: "https://drive.google.com/thumbnail?id=1uhz1T3ID-FuFDdSredRLyc6WZ7aDwfN4&sz=w500",
-    churchBg: "https://images.unsplash.com/photo-1548625361-195fe578ae1b?w=800",
-    venueBg: "https://images.unsplash.com/photo-1519167758481-83f550bb49b3?w=800"
+    // You can also put images in your GitHub repo and use "corner1.png", "logo.png", etc.
+    logoUrl: "logo.png", 
+    corner1Url: "https://drive.google.com/thumbnail?id=1Y-E_z3H5K8B4j7w_123456&sz=w500",
+    corner2Url: "https://drive.google.com/thumbnail?id=1Y-E_z3H5K8B4j7w_123456&sz=w500",
+    couplePhotoUrl: "https://drive.google.com/thumbnail?id=1P_couple_photo_id&sz=w600",
+    showCouplePhoto: false, // Set to true to show the circular portrait ring
+    churchBgUrl: "https://images.unsplash.com/photo-1548625361-195fe578ae1b?w=800",
+    venueBgUrl: "https://images.unsplash.com/photo-1519167758481-83f550bb49b3?w=800",
+    splashImageUrl: "https://drive.google.com/thumbnail?id=1splash_banner_id&sz=w800"
   }
 };
